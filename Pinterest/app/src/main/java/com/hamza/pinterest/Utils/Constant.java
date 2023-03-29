34ae1ee1022fb17f3643fe3b8211941a4a974e79
hamzamak <makhkhas.hamza@gmail.com>
2023-03-29 19:06:35 +0000

    change server baseUrl location

--- Pinterest/app/src/main/java/com/hamza/pinterest/Utils/Constant.java
+++ Pinterest/app/src/main/java/com/hamza/pinterest/Utils/Constant.java
@@ -3,5 +3,5 @@ package com.hamza.pinterest.Utils;
 public class Constant {
 
     public final static String KEY_SHARED_JWT= "KEY_SHARED_JWT" ;
-    public final static String BASE_URL= "http://192.168.1.53:5000/api/v1/" ;
+    public final static String BASE_URL= "https://pinterest.onrender.com/api/v1/" ; // in development use http://<your_ip_adress:5000/api/v1
 }
